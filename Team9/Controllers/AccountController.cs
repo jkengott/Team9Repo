84450ba5083a53ca484d84828321b97e6df5b3dc
Clay Kelly <cmk.clay@yahoo.com>
2016-11-28 14:06:11 -0600

    fixed the Account problem
    **you may have to register again i you already did, I had to delete
    accounts**

--- Team9/Controllers/AccountController.cs
+++ Team9/Controllers/AccountController.cs
@@ -121,7 +121,8 @@ namespace Team9.Controllers
             {
                 //TODO: Add fields to user here so they will be saved to the database
                 //Create a new user with all the properties you need for the class
-                var user = new AppUser { UserName = model.FName, Email = model.Email };
+                var user = new AppUser { UserName = model.Email, Email = model.Email, FName = model.FName,
+                LName = model.LName, CCType1 = model.CCType1, CCType2 = model.CCType2, SSN = model.SSN};
 
                 //Add the new user to the database
                 var result = await UserManager.CreateAsync(user, model.Password);
